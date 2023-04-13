6dd173d623172d34f2a4b9141da073d189a47d6f
Matilde <matilde.lucchesi3@stud.unifi.it>
2023-04-14 01:20:44 +0200

    Fix some bug and add search button and search dropdown menu

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
 document.addEventListener('click', e=>{
     const isDropdownButton = e.target.matches('[data-dropdown-button]');
+    const overlay = document.querySelector('.overlay');
     if(!isDropdownButton && e.target.closest('[data-dropdown]') != null) return;
 
     let currentDropdown
@@ -21,11 +22,38 @@ document.addEventListener('click', e=>{
         currentDropdown = e.target.closest('[data-dropdown]');
         currentDropdown.classList.toggle('active');
         console.log('Dropdown:', currentDropdown, 'active:', currentDropdown.classList.contains('active'));
+
+        if (overlay.style.display === 'block') {
+            overlay.style.display = 'none';
+        } else {
+            overlay.style.display = 'block';
+        }
     }
 
     document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
         if (dropdown === currentDropdown) return;
         dropdown.classList.remove('active');
         console.log('Dropdown:', dropdown, 'active:', dropdown.classList.contains('active'));
+        overlay.style.display = 'none';
     });
+
+});
+
+document.addEventListener('click', e=>{
+    const isDropdownSearch = e.target.matches('[search-dropdown-button]');
+    if(!isDropdownSearch && e.target.closest('[search-dropdown]') != null) return;
+
+    let currentDropdown
+    if(isDropdownSearch){
+        currentDropdown = e.target.closest('[search-dropdown]');
+        currentDropdown.classList.toggle('active');
+        console.log('Dropdown:', currentDropdown, 'active:', currentDropdown.classList.contains('active'));
+    }
+
+    document.querySelectorAll("[search-dropdown].active").forEach(dropdown => {
+        if (dropdown === currentDropdown) return;
+        dropdown.classList.remove('active');
+        console.log('Dropdown:', dropdown, 'active:', dropdown.classList.contains('active'));
+    });
+
 });
